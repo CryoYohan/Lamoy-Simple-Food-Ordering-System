22e55a236099559f57dffd64d39681ce92a31894
MarlouTadlip <tadlipmarlou@gmail.com>
2025-09-09 23:14:56 +0800

    Initial Backend Commit w/ Models/EntityFrameWork/Database

---
+++ backend/Program.cs
@@ -0,0 +1,25 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Application DB Context
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
